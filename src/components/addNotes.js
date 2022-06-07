2da50fbca37a3ecb1e092f687792c608fb6ac191
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-07 01:43:24 -0600

    Update addNotes.js

--- src/components/addNotes.js
+++ src/components/addNotes.js
@@ -5,7 +5,7 @@ const addNotes = () => {
   setnotesText(event.target.value);
  }
  return (<div className='notes add'>
-         <textarea rows='8' cols='10' placeholder='Type your new note here... onChange={noteEdit}]
+         <textarea rows='8' cols='10' placeholder='Type your new note here... value={notesText} onChange={noteEdit}]
          </textarea>
          <div className="footnotes"
          <small> </small>
