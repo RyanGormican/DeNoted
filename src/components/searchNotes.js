ce52eae0fd43872b4079a0b4f268b1de2481000b
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-07 02:25:53 -0600

    Update searchNotes.js

--- src/components/searchNotes.js
+++ src/components/searchNotes.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MDSearch } from'react-icons/md';
 const searchNotes =() = {
    return <div className='searchNotes'>
    <MDSearch className='search-icons' size='1.3em' />
