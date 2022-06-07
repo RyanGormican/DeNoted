4e5fe64a6f4a5b95e985b0f7fac91f74c0475c13
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-07 03:30:44 -0600

    Update searchNotes.js

--- src/components/searchNotes.js
+++ src/components/searchNotes.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { MDSearch } from "react-icons/md";
+// import { MDSearch } from "react-icons/md";
 const searchNotes =({ setSearch }) = {
    return <div className='searchNotes'>
-   <MDSearch className='search-icons' size='1.3em' />
+  // <MDSearch className='search-icons' size='1.3em' />
    <input  onChange={(event) => setSearch(event.target.value)}type="text" placehold='type to search for a note...' />
    </div>;
 };
