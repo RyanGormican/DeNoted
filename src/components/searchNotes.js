c6c1002a7475d31deb3bacbac6973194fa7c5904
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-07 02:25:21 -0600

    Update searchNotes.js

--- src/components/searchNotes.js
+++ src/components/searchNotes.js
@@ -1,5 +1,7 @@
 import React from 'react';
 const searchNotes =() = {
-   return <div className="searchNotes"></div>;
+   return <div className='searchNotes'>
+   <MDSearch className='search-icons' size='1.3em' />
+   </div>;
 };
 export default searchNotes;
