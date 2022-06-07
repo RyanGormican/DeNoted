790cd3977064d392bfdf2baabc3f8d14def53757
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-07 03:31:57 -0600

    Update denoteNote.js

--- src/components/denoteNote.js
+++ src/components/denoteNote.js
@@ -5,7 +5,7 @@ const denoteNote = ({ noteId, noteText, noteDate, removetheNote }) => {
     <span> {noteText } </span>
     <div className='footnotes'>
       <small> {noteDate} </small>
-    <Button onClick={() => removetheNote(noteId)} className='deletenotes' size='1.3em' ></Button>
+    <Button onClick={() => removetheNote(noteId)} className='deletenotes' size='1.3em' > X </Button>
     </div>
     
     </div>
