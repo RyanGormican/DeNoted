f515317b91e45e0a38fbca379c587d71e967ef04
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-07 01:46:36 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { useState } from 'react';
 import listNotes from './components/listNotes';
 const App = () => {
     const [note, setNote] = useState([{ noteId: nanoid(),  noteText: "Edit the note!", noteDate:""}]);
+    
+    const addNote = (text) => {
+        
+    }
     return (<div className="containNotes">
         <listNotes theNotes={note}/>
         </div>;
