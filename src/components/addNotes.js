7592503e3e1311e486c3b650894966f58f9513b1
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-07 01:42:58 -0600

    Update addNotes.js

--- src/components/addNotes.js
+++ src/components/addNotes.js
@@ -1,6 +1,11 @@
+import { useState } from 'react';
 const addNotes = () => {
+     const [notesText, setnotesText] = useState('');
+ const noteEdit = (event) => {
+  setnotesText(event.target.value);
+ }
  return (<div className='notes add'>
-         <textarea rows='8' cols='10' placeholder='Type your new note here...]
+         <textarea rows='8' cols='10' placeholder='Type your new note here... onChange={noteEdit}]
          </textarea>
          <div className="footnotes"
          <small> </small>
