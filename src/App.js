7a898312f4f3096f094869adad25502b80f0ead4
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-07 01:51:52 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import listNotes from './components/listNotes';
 const App = () => {
     const [note, setNote] = useState([{ noteId: nanoid(),  noteText: "Edit the note!", noteDate:""}]);
     
-    const addNote = (text) => {
-        
+    const addNote = (note) => {
+        const date = new Date();
+        const newNote = {
+            noteId: nanoid(),
+            noteText note,
+            noteDate: date.toLocaleDateString();
     }
+    const newNotes = [...note, newNote];
+    };
     return (<div className="containNotes">
         <listNotes theNotes={note} addtheNote={addNote}/>
         </div>;
