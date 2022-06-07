b33819ba54592f234b60cc4c75043dfb7e5e36a0
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-07 04:17:42 -0600

    Update NoteHeader.js

--- src/components/NoteHeader.js
+++ src/components/NoteHeader.js
@@ -1,7 +1,7 @@
 import React from 'react';
 const NoteHeader = ({ toggleDarkMode }) => {
     return (
-    <div className="header">
+    <div className='header'>
     <h1>DeNoted</h1>
     <button onClick={()=> toggleDarkMode((previousdarkMode)=> !previousdarkMode)}className='saveNote'> Toggle Dark Mode </button>
     </div>
