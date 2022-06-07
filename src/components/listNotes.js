c95eb25e1abacdffd9700e84611a340186542fea
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-07 02:09:52 -0600

    Update listNotes.js

--- src/components/listNotes.js
+++ src/components/listNotes.js
@@ -3,9 +3,9 @@ import addNotes from './addNotes';
 const listNotes = ({ theNotes, addtheNote, removetheNote }) => {
   return (
     <div className='listnotes'>
-    {theNotes.map((note)=>   <denoteNote noteId={note.noteId} noteText={note.noteText} noteDate={note.noteDate} />)}
+    {theNotes.map((note)=>   <denoteNote noteId={note.noteId} noteText={note.noteText} noteDate={note.noteDate}removetheNote={removetheNote} />)}
   
-<addNotes addtheNote removetheNote/>
+<addNotes addtheNote={addtheNote}/>
 </div>;
     )
 }
