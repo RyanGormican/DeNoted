7fc8890ac7ad3a2b27e985a91ab9c5adb1e7d798
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-07 01:30:18 -0600

    Update addNotes.js

--- src/components/addNotes.js
+++ src/components/addNotes.js
@@ -1,3 +1,6 @@
 const addNotes = () => {
- return (<div></div>) 
+ return (<div className='notes add'>
+         <textarea rows='8' cols='10' placeholder='Type your new note here...]
+         </textarea>
+         </div>) 
 }
