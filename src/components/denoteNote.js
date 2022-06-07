7130daa52e4f46d92ce4b8e48d9f117cbf1f7973
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-07 03:31:35 -0600

    Update denoteNote.js

--- src/components/denoteNote.js
+++ src/components/denoteNote.js
@@ -1,11 +1,11 @@
-import { TiDelete } from "react-icons/md";
+// import { TiDelete } from "react-icons/md";
 const denoteNote = ({ noteId, noteText, noteDate, removetheNote }) => {
   return(
     <div className='notes'>
     <span> {noteText } </span>
     <div className='footnotes'>
       <small> {noteDate} </small>
-    <TiDelete onClick={() => removetheNote(noteId)} className='deletenotes' size='1.3em' />
+    <Button onClick={() => removetheNote(noteId)} className='deletenotes' size='1.3em' ></Button>
     </div>
     
     </div>
