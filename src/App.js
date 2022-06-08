d9ca9d938c15bce7f5402b2253bc865783070935
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-08 01:14:15 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import NoteHeader from './components/NoteHeader';
 
 
 const App = () => {
-    const [notes, setNotes] = useState([{ noteId: nanoid(),  noteText:'Welcome to DeNoted!', noteDate:'06/07/2022'}]);
+    
+            const date = new Date();
+    const [notes, setNotes] = useState([{ noteId: nanoid(),  noteText:'Welcome to DeNoted!', noteDate:date.toLocaleDateString()}]);
     const [search, setSearch] = useState(''); 
     var Changed = false;
     if (JSON.parse(localStorage.getItem('denoted-data-darkmode')) === "true")
