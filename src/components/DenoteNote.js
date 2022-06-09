d98ad258f15fb6616d7e79fe797951d6b90ce0f4
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-08 21:19:37 -0600

    Update DenoteNote.js

--- src/components/DenoteNote.js
+++ src/components/DenoteNote.js
@@ -1,9 +1,13 @@
-const DenoteNote = ({ noteId, noteText, noteDate, removetheNote }) => {
+const DenoteNote = ({ noteId, noteText, noteDate, noteColor, removetheNote,setNotes }) => {
+  const changeColor = (event) => {
+   setNotes(
+  }
   return(
-    <div className='notes' id={noteId}>
+    <div className='notes' id={noteId} style="background-color: " + {noteColor} + ";">
     <span> {noteText } </span>
     <div className='footnotes'>
       <small> {noteDate} </small>
+     <input type="color" onChange={changeColor} id="noteColorS" name="noteColor"  value={noteColor}>
     <button onClick={() => removetheNote(noteId)} className='deletenotes' size='1.3em' > X </button>
     </div>
     
