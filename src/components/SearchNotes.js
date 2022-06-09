1f6dc4ecdfcbb6935c439c241f8cafb409bc4a74
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-06-09 16:54:29 -0600

    Update SearchNotes.js

--- src/components/SearchNotes.js
+++ src/components/SearchNotes.js
@@ -3,7 +3,7 @@ const SearchNotes =({ setSearch }) => {
    return (
    <div className='searchNotes'>
    <input  onChange={(event) => 
-   setSearch(event.target.value)} type='text' placeholder='type to search for a note...' />
+   setSearch(event.target.value)} type='text' placeholder='Type to search for a note...' />
    </div>
 );
 };
