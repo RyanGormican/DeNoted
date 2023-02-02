a6758e2bc936b7d8cb7733ec6368f55a638b08a1
RyanGormican <ryan.gormican@gmail.clom>
2023-02-01 22:19:30 -0600

    neutral color

--- src/components/NoteHeader.js
+++ src/components/NoteHeader.js
@@ -5,12 +5,14 @@ const NoteHeader = ({ toggleDarkMode }) => {
     return (
     <div className='header'>
     <h1>DeNoted</h1>
+    <span>
     <a href="https://www.linkedin.com/in/ryangormican/">
 		<Icon icon="mdi:linkedin" color="#0e76a8" width="40" />
 	</a>
 	<a href="https://github.com/RyanGormican/DeNoted">
-		<Icon icon="mdi:github" color="white" width="40" />
+		<Icon icon="mdi:github" color="#e8eaea" width="40" />
 	</a>
+    </span>
     <button onClick={()=> toggleDarkMode((previousdarkMode)=> !previousdarkMode)}className='saveNote'> Toggle Dark Mode </button>
     </div>
     )
