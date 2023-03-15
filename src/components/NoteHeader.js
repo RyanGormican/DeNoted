ee7bb3815844afe2e7d88c4a6ad0656a06437ba8
RyanGormican <ryan.gormican@gmail.com>
2023-03-15 00:22:31 -0600

    a
    a

--- src/components/NoteHeader.js
+++ src/components/NoteHeader.js
@@ -12,6 +12,9 @@ const NoteHeader = ({ toggleDarkMode }) => {
 	<a href="https://github.com/RyanGormican/DeNoted">
 		<Icon icon="mdi:github" color="#e8eaea" width="40" />
 	</a>
+    <a href="https://ryangormicanportfoliohub.vercel.app/">
+		<Icon icon="teenyicons:computer-outline" color="#199c35" width="40" />
+	</a>
     </span>
     <button onClick={()=> toggleDarkMode((previousdarkMode)=> !previousdarkMode)}className='saveNote'> Toggle Dark Mode </button>
     </div>
